45e3d3801be3777857ef482378f1e380072fa726
znn1980 <znn1980@163.com>
2025-03-01 18:33:51 +0800

    弃用OkHttpClient

--- src/main/java/web/digital/human/HumanClient.java
+++ src/main/java/web/digital/human/HumanClient.java
@@ -4,7 +4,11 @@ import com.baidubce.qianfan.Qianfan;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import okhttp3.OkHttpClient;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -23,8 +27,18 @@ public class HumanClient {
     }
 
     @Bean
-    public OkHttpClient httpClient() {
-        return new OkHttpClient.Builder().build();
+    public CloseableHttpClient httpClient() {
+        return HttpClients.custom().useSystemProperties()
+                .setConnectionManager(new PoolingHttpClientConnectionManager() {{
+                    this.setMaxTotal(10);
+                    this.setDefaultMaxPerRoute(10);
+                }})
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(Timeout.ofSeconds(10))
+                        .setConnectionRequestTimeout(Timeout.ofSeconds(10))
+                        .setConnectionRequestTimeout(Timeout.ofSeconds(10))
+                        .build())
+                .build();
     }
 
     @Bean
--- src/main/java/web/digital/human/HumanController.java
+++ src/main/java/web/digital/human/HumanController.java
@@ -7,17 +7,22 @@ import com.baidubce.qianfan.model.chat.v2.request.RequestV2;
 import com.baidubce.qianfan.model.chat.v2.response.ResponseV2;
 import com.baidubce.qianfan.model.chat.v2.response.StreamResponseV2;
 import com.google.gson.Gson;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okio.ByteString;
-import okio.Okio;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.HttpEntities;
+import org.apache.hc.core5.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,13 +35,15 @@ import java.io.IOException;
 @Controller
 public class HumanController {
     private final static Logger LOGGER = LoggerFactory.getLogger(HumanController.class);
+    private final static String ASR_URL = "https://vop.baidu.com/pro_api?token=%s&cuid=SC1234567890&dev_pid=80001";
+    private final static String TOKEN_URL = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s";
     private final HumanConfig config;
     private final Gson gson;
     private final QianfanV2 client;
-    private final OkHttpClient httpClient;
+    private final CloseableHttpClient httpClient;
     private final Credentials credentials = new Credentials();
 
-    public HumanController(HumanConfig config, Gson gson, Qianfan client, OkHttpClient httpClient) {
+    public HumanController(HumanConfig config, Gson gson, Qianfan client, CloseableHttpClient httpClient) {
         this.config = config;
         this.gson = gson;
         this.client = client.v2();
@@ -45,25 +52,25 @@ public class HumanController {
 
     @GetMapping(value = "/api/credentials", produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
-    public String credentials() throws IOException {
+    public String credentials() throws IOException, ParseException {
         this.refreshCredentials();
         return this.credentials.getAccessToken();
     }
 
+
     @PostMapping(value = "/api/speech/recognitions", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String speechRecognitions(@RequestBody byte[] vop) throws IOException {
+    public String speechRecognitions(@RequestBody byte[] vop) throws IOException, ParseException {
         this.refreshCredentials();
-        try (Response response = this.httpClient.newCall(new Request.Builder()
-                .header("format", "pcm").header("rate", "16000")
-                .post(okhttp3.RequestBody.create(okhttp3.MediaType.parse("audio/pcm;rate=16000")
-                        , Base64.decodeBase64(vop)))
-                .url(String.format("https://vop.baidu.com/pro_api?token=%s&cuid=SC1234567890&dev_pid=80001"
-                        , this.credentials.getAccessToken()))
-                .build()).execute()) {
-            ByteString byteString = ByteString.of(response.body().bytes());
-            LOGGER.info("ASR：{}", byteString.utf8());
-            return byteString.utf8();
+        HttpPost httpPost = new HttpPost(String.format(ASR_URL, this.credentials.getAccessToken()));
+        httpPost.addHeader("format", "pcm");
+        httpPost.addHeader("rate", "16000");
+        httpPost.setEntity(HttpEntities.create(Base64.decodeBase64(vop)
+                , ContentType.create("audio/pcm", new BasicNameValuePair("rate", "16000"))));
+        try (CloseableHttpResponse response = this.httpClient.execute(httpPost)) {
+            String json = EntityUtils.toString(response.getEntity());
+            LOGGER.info("ASR：{}", json);
+            return json;
         }
     }
 
@@ -73,37 +80,37 @@ public class HumanController {
         if (request.isStream()) {
             StreamIterator<StreamResponseV2> response = this.client.chatCompletionStream(request);
             return ResponseEntity.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(os -> {
-                while (response.hasNext()) {
-                    StreamResponseV2 stream = response.next();
-                    if (stream == null) {
-                        continue;
+                try {
+                    while (response.hasNext()) {
+                        StreamResponseV2 stream = response.next();
+                        if (stream == null) {
+                            break;
+                        }
+                        String json = this.gson.toJson(stream);
+                        LOGGER.info("答：{}", json);
+                        StreamUtils.copy((json + "\r\n\r\n").getBytes(), os);
                     }
-                    ByteString byteString = ByteString.encodeUtf8(this.gson.toJson(stream));
-                    LOGGER.info("答：{}", byteString.utf8());
-                    Okio.buffer(Okio.sink(os)).write(byteString)
-                            .writeUtf8("\r\n").writeUtf8("\r\n").flush();
+                } finally {
+                    response.close();
                 }
-                response.close();
             });
         } else {
             ResponseV2 response = this.client.chatCompletion(request);
-            ByteString byteString = ByteString.encodeUtf8(this.gson.toJson(response));
-            LOGGER.info("答：{}", byteString.utf8());
+            String json = this.gson.toJson(response);
+            LOGGER.info("答：{}", json);
             return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(os ->
-                    Okio.buffer(Okio.sink(os)).write(byteString));
+                    StreamUtils.copy(json.getBytes(), os));
         }
     }
 
-    void refreshCredentials() throws IOException {
+    void refreshCredentials() throws IOException, ParseException {
         if (!StringUtils.hasText(this.credentials.getAccessToken())
                 || this.credentials.getExpiresIn() < System.currentTimeMillis()) {
-            try (Response response = this.httpClient.newCall(new Request.Builder()
-                    .url(String.format("https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s"
-                            , config.getBaidu().getAppKey(), config.getBaidu().getAppSecretKey()))
-                    .build()).execute()) {
-                ByteString byteString = ByteString.of(response.body().bytes());
-                LOGGER.info("TOKEN：{}", byteString.utf8());
-                Credentials credentials = this.gson.fromJson(byteString.utf8(), Credentials.class);
+            HttpGet httpGet = new HttpGet(String.format(TOKEN_URL, config.getBaidu().getAppKey(), config.getBaidu().getAppSecretKey()));
+            try (CloseableHttpResponse response = this.httpClient.execute(httpGet)) {
+                String json = EntityUtils.toString(response.getEntity());
+                LOGGER.info("TOKEN：{}", json);
+                Credentials credentials = this.gson.fromJson(json, Credentials.class);
                 this.credentials.setError(credentials.getError());
                 this.credentials.setErrorDescription(credentials.getErrorDescription());
                 this.credentials.setAccessToken(credentials.getAccessToken());
